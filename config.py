ce20006f931e397f52545c00cd1532e99e1e6e97
Chee Hong <c.h.wong2606@gmail.com>
2022-01-08 17:40:30 +0800

    cosmetic 2

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from configparser import ConfigParser
 
-section_to_use = "elephantsql" #change this accordingly
+database_to_use = "elephantsql" #change this accordingly
 
-def config(filename="db.ini", section=section_to_use):
+def config(filename="db.ini", section=database_to_use):
     parser = ConfigParser()
     parser.read(filename)
     db = {}
@@ -17,7 +17,7 @@ def config(filename="db.ini", section=section_to_use):
     return db
     
 def sqlalchemy_engine_str(filename="db.ini", dialect="postgresql"):
-    config_dict = config(filename=filename, section=section_to_use)
+    config_dict = config(filename=filename, section=database_to_use)
     config_dict['dialect'] = dialect
     
     output = '{dialect}://{user}:{password}@{host}:5432/{database}'.format(**config_dict)
